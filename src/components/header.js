3fa34653a01e81acb87a62eeeb2b73e327715140
Kendall Whitman <kendallw@protonmail.com>
2019-03-11 22:18:30 -0400

    Update design colors

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
-import PropTypes from "prop-types"
-import React from "react"
+import React from 'react'
+import PropTypes from 'prop-types'
 
 import Github from '../images/github.svg'
 import Gogs from '../images/git.svg'
@@ -8,49 +8,51 @@ import LinkedIn from '../images/linkedin.svg'
 import Mastodon from '../images/mastodon.svg'
 import Instagram from '../images/instagram.svg'
 
-document.body.onscroll = () => {
-  const header = document.querySelector('.header')
-  window.pageYOffset > 10
-    ? header.classList.add('header--shadow')
-    : header.classList.remove('header--shadow')
-}
-
-const Header = ({ title, subtitle }) => (
-  <header className="header">
-    <a href="/" className="logo">
-      <span className="logo__title">{title}</span>
-      <span className="logo__subtitle">{subtitle}</span>
+const Header = ({ title, subtitle, toggleMenu, menuActive }) => (
+  <header className='header'>
+    <a href='/' className='logo'>
+      <span className='logo__title'>{title}</span>
+      <span className='logo__subtitle'>{subtitle}</span>
     </a>
-    <nav className="nav">
-      <ul className="nav__list">
-        <li className="nav__item">
-          <a href="https://github.com/KendallWhitman" title="GitHub" className="nav__link">
+
+    <span onClick={() => { toggleMenu() }} className='mobile-menu'>MENU</span>
+
+    <nav className={`nav ${menuActive ? 'nav--active' : ''}`}>
+      <ul className='nav__list'>
+        <li className='nav__item'>
+          <a href='https://github.com/KendallWhitman' title='GitHub' className='nav__link'>
             <Github className='nav__icon' />
+            <span class='nav__text'>GitHub</span>
           </a>
         </li>
-        <li className="nav__item">
-          <a href="https://gogs.thewhitmans.cloud/explore" title="Gogs" className="nav__link">
+        <li className='nav__item'>
+          <a href='https://gogs.thewhitmans.cloud/explore' title='Gogs' className='nav__link'>
             <Gogs className='nav__icon' />
+            <span class='nav__text'>Gogs</span>
           </a>
         </li>
-        <li className="nav__item">
-          <a href="https://codepen.io/KendallWhitman" title="CodePen" className="nav__link">
+        <li className='nav__item'>
+          <a href='https://codepen.io/KendallWhitman' title='CodePen' className='nav__link'>
             <CodePen className='nav__icon' />
+            <span class='nav__text'>CodePen</span>
           </a>
         </li>
-        <li className="nav__item">
-          <a href="https://www.linkedin.com/in/kendall-whitman-9465ba11b" title="LinkedIn" className="nav__link">
+        <li className='nav__item'>
+          <a href='https://www.linkedin.com/in/kendall-whitman-9465ba11b' title='LinkedIn' className='nav__link'>
             <LinkedIn className='nav__icon' />
+            <span class='nav__text'>LinkedIn</span>
           </a>
         </li>
-        <li className="nav__item">
-          <a href="https://linuxrocks.online/@AsteroidMiner" title="Mastodon" className="nav__link">
+        <li className='nav__item'>
+          <a href='https://linuxrocks.online/@AsteroidMiner' title='Mastodon' className='nav__link'>
             <Mastodon className='nav__icon' />
+            <span class='nav__text'>Mastodon</span>
           </a>
         </li>
-        <li className="nav__item">
-          <a href="https://www.instagram.com/kendallawhitman/" title="Instagram" className="nav__link">
+        <li className='nav__item'>
+          <a href='https://www.instagram.com/kendallawhitman/' title='Instagram' className='nav__link'>
             <Instagram className='nav__icon' />
+              <span class='nav__text'>Instagram</span>
           </a>
         </li>
       </ul>
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,39 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
 import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
 import '../styles/styles.scss'
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-            subtitle
-          }
-        }
-      }
-    `}
-    render={data => {
-      const title = data.site.siteMetadata.title
-      const subtitle = data.site.siteMetadata.subtitle
-      return (
-        <>
-          <Helmet>
-            <title>{title} | {subtitle}</title>
-            <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Oswald:400,500,600' />
-          </Helmet>
-          <Header title={title} subtitle={subtitle} />
-          <main className='main'>{children}</main>
-        </>
-      )
-    }}
-  />
-)
+class Layout extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      menuActive: false,
+    }
+    this._toggleMenu = this._toggleMenu.bind(this)
+  }
+
+  _toggleMenu () {
+    this.setState({ menuActive: !this.state.menuActive })
+  }
+
+  render () {
+    const title = 'Kendall Whitman'
+    const subtitle = 'Front End Developer'
+    const { children } = this.props
+    const { menuActive } = this.state
+
+    return (
+      <>
+        <Helmet>
+          <title>{title} | {subtitle}</title>
+          <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Oswald:400,500,600' />
+        </Helmet>
+        <Header
+          title={title}
+          subtitle={subtitle}
+          toggleMenu={this._toggleMenu}
+          menuActive={menuActive}
+        />
+        <main className='main'>{children}</main>
+      </>
+    )
+  }
+
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
