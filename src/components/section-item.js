645a312dadb380bee9d3db4fd2a440be78c07eba
Kendall Whitman <kendallw@protonmail.com>
2019-03-14 23:32:34 -0400

    Code cleanup

--- src/components/section-item.js
+++ src/components/section-item.js
@@ -3,37 +3,35 @@ import React from 'react'
 const SectionItem = props => {
   const { grid, i, item } = props
 
-  if (!grid) {
-    return (
-      <div key={i} className='section-item'>
-        <item.icon className='section-item__icon section-item__icon--large' />
-        <h3 className='section-item__title'>{item.name}</h3>
-        <div className='section-item__container'>
-          <p className='section-item__text section-item__text--spaced'>
-            {item.description}
-          </p>
-          <ul className="section-item__list">
-            {item.links && item.links.map((link, i) => (
-              <li key={i} className="section-item__item">
-                <a href={link.link} className='link'>{link.name}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    )
-  } else {
-    return (
-      <div key={i} className='section-item'>
-        {item.icon &&
-          <item.icon className='section-item__icon' />
-        }
+  return (
+    <div key={i} className='section-item'>
+      {item.icon &&
+        <item.icon className={`section-item__icon ${!grid ? 'section-item__icon--large': ''}`} />
+      }
+      {grid &&
         <p className='section-item__text section-item__text--centered'>
           {item.name}
         </p>
-      </div>
-    )
-  }
+      }
+      {!grid &&
+        <>
+          <h3 className='section-item__title'>{item.name}</h3>
+          <div className='section-item__container'>
+            <p className='section-item__text section-item__text--spaced'>
+              {item.description}
+            </p>
+            <ul className="section-item__list">
+              {item.links && item.links.map((link, i) => (
+                <li key={i} className="section-item__item">
+                  <a href={link.link} className='link'>{link.name}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      }
+    </div>
+  )
 }
 
 export default SectionItem
