529838cf68fdbd45552c036cb0635c675fa48a80
Kendall Whitman <kendallw@protonmail.com>
2019-05-17 02:59:06 +0000

    Update hero message

--- src/data/hero-data.js
+++ src/data/hero-data.js
@@ -1,12 +1,6 @@
-const yearsInWebDev = () => {
-  const started = 2014
-  const current = new Date().getFullYear()
-  return current - started
-}
-
 const heroData = {
   title: `Hi! I'm Kendall.`,
-  message: `A Front End Developer with ${yearsInWebDev()} years of experience in a wide range of disciplines.`,
+  message: `I'm a front-end developer who helps companies build responsive, scalable applications focusing on code simplicity and straight forward solutions so they can bring their ideas to market faster.`,
 }
 
 export default heroData
