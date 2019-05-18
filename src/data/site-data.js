fa9cfa97e19675476c3c3e342316670b633dfc01
Kendall Whitman <kendallw@protonmail.com>
2019-05-18 15:35:01 -0400

    Add pgp key and fingerprint

--- src/data/site-data.js
+++ src/data/site-data.js
@@ -2,6 +2,8 @@ const siteData = {
   title: 'Kendall Whitman',
   subtitle: 'Front End Developer',
   email: 'kendallwhitman@gmail.com',
+  pgpKey: 'https://next.thewhitmans.cloud/index.php/s/WBJrTaeggTAogTx/download',
+  pgpFingerprint: '1959 A028 55C7 C66E EB53 2117 C5EA 890B 0C71 3D6C',
   resumes: [
     {
       link: 'https://next.thewhitmans.cloud/index.php/s/qyARtBYmGK4srcz/download',
