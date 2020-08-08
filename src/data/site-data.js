49fb861db3a9be68335406d9ba3282d2a4731626
Kendall Whitman <kendallw@protonmail.com>
2020-08-08 08:39:24 -0500

    update resume links

--- src/data/site-data.js
+++ src/data/site-data.js
@@ -4,11 +4,11 @@ const siteData = {
   email: 'kendallwhitman@gmail.com',
   resumes: [
     {
-      link: 'https://next.thewhitmans.cloud/s/RABLt3fxxbe7gJf/download',
+      link: 'https://next.thewhitmans.cloud/s/5MR8NMBH3kswXb9/download',
       name: 'PDF',
     },
     {
-      link: 'https://next.thewhitmans.cloud/s/rrpFJRPgXYNYmSt/download',
+      link: 'https://next.thewhitmans.cloud/s/6mgG6WrqXWP789o/download',
       name: 'Word',
     },
   ]
