fca2716786e2f59f60c94dd7bc9dd6cdd1368b90
Kendall Whitman <kendallw@protonmail.com>
2024-01-24 10:48:54 -0600

    master: update jobs data

--- src/data/portfolio-data.js
+++ src/data/portfolio-data.js
@@ -3,18 +3,16 @@ import Alkami from '../images/alkami.svg'
 import DoctorLogic from '../images/doctorlogic.svg'
 import Realtor from '../images/realtor.svg'
 import Complyify from '../images/complyify.svg'
-import CampingWorld from '../images/campingworld.svg';
 import NBC from '../images/nbc.svg'
-import OA from '../images/OA.svg'
 
 const PortfolioData = [
   {
     icon: NBC,
     name: `NBC News`,
-    duration: `September 2021 - Present`,
-    description: `I'm currently apart of the Articles & Video team creating 
-    features within articles and maintaining a consistent experience with our 
-    VOD and live content. We successfully launched CNBC's Investing Club 
+    duration: `2021 - Present`,
+    description: `I'm currently apart of the Articles & Video team creating
+    features within articles and maintaining a consistent experience with our
+    VOD and live content. We successfully launched CNBC's Investing Club
     subscription and hosted the Berkshire Hathaway spring event.`,
     links: [
       {
@@ -24,26 +22,12 @@ const PortfolioData = [
     ],
   },
   {
-    icon: OA,
-    name: `Overlay Analytics`,
-    duration: `April 2020 - August 2021`,
-    description: `With Overlay Analytics I worked on the frontend team, 
-    collaborating with the backend, data, and design teams to build out a 
-    scalable UI interface.`,
-    links: [
-      {
-        name: 'OverlayAnalytics.com',
-        link: 'https://overlayanalytics.com'
-      }
-    ],
-  },
-  {
     icon: NBC,
     name: `CNBC`,
-    duration: `December 2020 - April 2020`,
-    description: `At CNBC, I worked on the Engagement team, building 
-    out new core functionality. This included new features and bugfixes in a 
-    scrum environment. The languages used here are React with SCSS, some 
+    duration: `2020 - 2021`,
+    description: `At CNBC, I worked on the Engagement team, building
+    out new core functionality. This included new features and bugfixes in a
+    scrum environment. The languages used here are React with SCSS, some
     GraphQL, and Jest/Enzyme for lots of testing.`,
     links: [
       {
@@ -53,22 +37,9 @@ const PortfolioData = [
     ],
   },
   {
-    icon: CampingWorld,
-    name: `Camping World`,
-    duration: `September 2020 - November 2020`,
-    description: `Worked with Camping World as a senior front end devloper,
-    building a platform for easy RV rentals.`,
-    links: [
-      {
-        name: 'CampingWorld.com',
-        link: 'https://campingworld.com'
-      }
-    ],
-  },
-  {
     icon: Complyify,
     name: `Complyify`,
-    duration: `May 2020 - August 2020`,
+    duration: `2020 - 2020`,
     description: `With Complyify I worked on creating and managing the frontend
     application for reviewing forgiveness loan requests regarding the Paycheck
     Protection Program. Most of my time was spent designing and building React
@@ -84,7 +55,7 @@ const PortfolioData = [
   {
     icon: Realtor,
     name: `Realtor.com (Move Inc.)`,
-    duration: `October 2019 to May 2020`,
+    duration: `2017 to 2020`,
     description: `Working remotely from Dallas, I worked on a small team of
     designers and developers creating and managing the Sell and MyHome pages of
     realtor.com. Most of my day is spent working with React, implementing API
@@ -99,26 +70,9 @@ const PortfolioData = [
     ],
   },
   {
-    icon: Doorsteps,
-    name: `Doorsteps.com (Move Inc.)`,
-    duration: `July 2017 to May 2019`,
-    description: `At Doorsteps, I worked on a small, cross functional team with
-    four other developers. We useed ReactJS, Redux, PostgreSQL, SCSS, and other
-    technologies to build out our front end with mostly NodeJS (We still had
-      a lone Ruby on Rails server) on the backend. While being a cross
-    functional team means I touch all parts of the application, my strengths
-    tend to lie in the front end, writing JavaScript and SCSS components.`,
-    links: [
-      {
-        name: 'Doorsteps.com',
-        link: 'https://www.doorsteps.com'
-      }
-    ],
-  },
-  {
     icon: Alkami,
     name: `Alkami`,
-    duration: `March 2016 to January 2017`,
+    duration: `2016 to 2017`,
     description: `Most of my responsibilities at Alkami were building out the
     front end for our banking clients. After working with the design team, I
     would take the finished design and build out the layouts needed. I would
@@ -135,7 +89,7 @@ const PortfolioData = [
   {
     icon: DoctorLogic,
     name: `DoctorLogic`,
-    duration: `July 2014 to March 2016`,
+    duration: `2014 to 2016`,
     description: `DoctorLogic was where I first started my web development
     career after my four year stint in IT support. I worked closely with the
     lead designer and would build out front page designs from Photoshop
